f08a1855a8afe8e5ceef5cbca3ad7a1c1a5b72df
Vivek Singh <vivekkrvks@gmail.com>
2023-05-13 16:32:52 +0530

    improved ui of setting screen

--- src/components/SettingComponent/index.js
+++ src/components/SettingComponent/index.js
@@ -4,8 +4,8 @@ import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const SettingComponent = () => {
-  const [selectedTheme, setSelectedTheme] = useState('system');
-  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [selectedTheme, setSelectedTheme] = useState('System');
+  const [selectedLanguage, setSelectedLanguage] = useState('English');
 
   const [isThemeModalVisible, setThemeModalVisible] = useState(false);
   const [isLanguageModalVisible, setLanguageModalVisible] = useState(false);
@@ -22,9 +22,7 @@ const SettingComponent = () => {
 
   return (
     <LinearGradient colors={['#FFFFFF', '#D9E4F5']} style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Settings</Text>
-      </View>
+    
       <View style={styles.option}>
         <View style={styles.optionContent}>
           <View style={styles.optionTextContainer}>
@@ -86,138 +84,267 @@ const SettingComponent = () => {
 </View>
 </TouchableOpacity>  
 {/* Theme Selection Modal */}
-  <Modal visible={isThemeModalVisible} animationType="slide">
-    <View style={styles.modalContainer}>
-      <Text style={styles.modalTitle}>Select Theme</Text>
-      <TouchableOpacity
-        style={styles.modalOption}
-        onPress={() => handleThemeSelection('Light')}
+<Modal visible={isThemeModalVisible} animationType="slide">
+  <View style={styles.modalContainer}>
+    <Text style={styles.modalTitle}>Select Theme</Text>
+    <TouchableOpacity
+      style={[
+        styles.modalOption,
+        selectedTheme === 'Light' && styles.selectedOption,
+      ]}
+      onPress={() => handleThemeSelection('Light')}
+    >
+      <Text
+        style={[
+          styles.modalOptionText,
+          selectedTheme === 'Light' && styles.selectedOptionText,
+        ]}
       >
-        <Text style={styles.modalOptionText}>Light</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.modalOption}
-        onPress={() => handleThemeSelection('Dark')}
+        Light
+        {selectedTheme === 'Light' && (
+          <Ionicons
+            name="checkmark-sharp"
+            size={24}
+            color="#00F"
+            style={styles.checkmarkIcon}
+          />
+        )}
+      </Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={[
+        styles.modalOption,
+        selectedTheme === 'Dark' && styles.selectedOption,
+      ]}
+      onPress={() => handleThemeSelection('Dark')}
+    >
+      <Text
+        style={[
+          styles.modalOptionText,
+          selectedTheme === 'Dark' && styles.selectedOptionText,
+        ]}
       >
-        <Text style={styles.modalOptionText}>Dark</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.modalOption}
-        onPress={() => handleThemeSelection('System')}
+        Dark
+        {selectedTheme === 'Dark' && (
+          <Ionicons
+            name="checkmark-sharp"
+            size={24}
+            color="#00F"
+            style={styles.checkmarkIcon}
+          />
+        )}
+      </Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={[
+        styles.modalOption,
+        selectedTheme === 'System' && styles.selectedOption,
+      ]}
+      onPress={() => handleThemeSelection('System')}
+    >
+      <Text
+        style={[
+          styles.modalOptionText,
+          selectedTheme === 'System' && styles.selectedOptionText,
+        ]}
       >
-        <Text style={styles.modalOptionText}>System</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.modalCloseButton}
-        onPress={() => setThemeModalVisible(false)}
-      >
-        <Text style={styles.modalCloseButtonText}>Close</Text>
-      </TouchableOpacity>
-    </View>
-  </Modal>
+        System
+        {selectedTheme === 'System' && (
+          <Ionicons
+            name="checkmark-sharp"
+            size={24}
+            color="#00F"
+            style={styles.checkmarkIcon}
+          />
+        )}
+      </Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+      style={styles.modalCloseButton}
+      onPress={() => setThemeModalVisible(false)}
+    >
+      <Text style={styles.modalCloseButtonText}>Close</Text>
+    </TouchableOpacity>
+  </View>
+</Modal>
+
 
   {/* Language Selection Modal */}
   <Modal visible={isLanguageModalVisible} animationType="slide">
-    <View style={styles.modalContainer}>
-      <Text style={styles.modalTitle}>Select Language</Text>
-      <TouchableOpacity
-        style={styles.modalOption}
-        onPress={() => handleLanguageSelection('English')}
-      >
-        <Text style={styles.modalOptionText}>English</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.modalOption}
-        onPress={() => handleLanguageSelection('Spanish')}
-      >
-        <Text style={styles.modalOptionText}>Spanish</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.modalOption}
-        onPress={() => handleLanguageSelection('French')}
-      >
-        <Text style={styles.modalOptionText}>French</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.modalCloseButton}
-        onPress={() => setLanguageModalVisible(false)}
-      >
-        <Text style={styles.modalCloseButtonText}>Close</Text>
-      </TouchableOpacity>
-    </View>
-  </Modal>
+      <View style={styles.modalContainer}>
+        <Text style={styles.modalTitle}>Select Language</Text>
+        <TouchableOpacity
+          style={[
+            styles.modalOption,
+            selectedLanguage === 'English' && styles.selectedOption,
+          ]}
+          onPress={() => handleLanguageSelection('English')}
+        >
+          <Text
+            style={[
+              styles.modalOptionText,
+              selectedLanguage === 'English' && styles.selectedOptionText,
+            ]}
+          >
+            English
+            {selectedLanguage === 'English' && (
+              <Ionicons
+                name="checkmark-sharp"
+                size={24}
+                color="#00F"
+                style={styles.checkmarkIcon}
+              />
+            )}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.modalOption,
+            selectedLanguage === 'Spanish' && styles.selectedOption,
+          ]}
+          onPress={() => handleLanguageSelection('Spanish')}
+        >
+          <Text
+            style={[
+              styles.modalOptionText,
+              selectedLanguage === 'Spanish' && styles.selectedOptionText,
+            ]}
+          >
+            Spanish
+            {selectedLanguage === 'Spanish' && (
+              <Ionicons
+                name="checkmark-sharp"
+                size={24}
+                color="#00F"
+                style={styles.checkmarkIcon}
+              />
+            )}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.modalOption,
+            selectedLanguage === 'French' && styles.selectedOption,
+          ]}
+          onPress={() => handleLanguageSelection('French')}
+        >
+          <Text
+            style={[
+              styles.modalOptionText,
+              selectedLanguage === 'French' && styles.selectedOptionText,
+            ]}
+          >
+            French
+            {selectedLanguage === 'French' && (
+              <Ionicons
+                name="checkmark-sharp"
+                size={24}
+                color="#00F"
+                style={styles.checkmarkIcon}
+              />
+            )}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.modalCloseButton}
+          onPress={() => setLanguageModalVisible(false)}
+        >
+          <Text style={styles.modalCloseButtonText}>Close</Text>
+        </TouchableOpacity>
+      </View>
+    </Modal>
+
 </LinearGradient>
 );
 };
 
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-},
-header: {
-paddingVertical: 20,
-paddingHorizontal: 15,
-borderBottomWidth: 1,
-borderBottomColor: '#ddd',
-},
-headerText: {
-fontSize: 24,
-fontWeight: 'bold',
-color: '#333',
-},
-option: {
-paddingVertical: 15,
-paddingHorizontal: 20,
-borderBottomWidth: 1,
-borderBottomColor: '#ddd',
-},
-optionContent: {
-flexDirection: 'row',
-justifyContent: 'space-between',
-alignItems: 'center',
-},
-optionTextContainer: {
-flexDirection: 'row',
-alignItems: 'center',
-},
-optionText: {
-marginLeft: 15,
-fontSize: 18,
-color: '#333',
-},
-selectedText: {
-color: '#666',
-fontSize: 16,
-},
-modalContainer: {
-flex: 1,
-paddingVertical: 50,
-paddingHorizontal: 20,
-},
-modalTitle: {
-fontSize: 24,
-fontWeight: 'bold',
-marginBottom: 20,
-color: '#333',
-},
-modalOption: {
-paddingVertical: 15,
-borderBottomWidth: 1,
-borderBottomColor: '#ddd',
-},
-modalOptionText: {
-fontSize: 18,
-color: '#333',
-},
-modalCloseButton: {
-marginTop: 20,
-alignSelf: 'center',
-},
-modalCloseButtonText: {
-fontSize: 18,
-color: '#333',
-fontWeight: 'bold',
-},
-});
+    container: {
+      flex: 1,
+    },
+    header: {
+      paddingVertical: 20,
+      paddingHorizontal: 15,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+    headerText: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: '#333',
+    },
+    option: {
+      paddingVertical: 15,
+      paddingHorizontal: 20,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+    optionContent: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    optionTextContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    optionText: {
+      marginLeft: 15,
+      fontSize: 18,
+      color: '#333',
+    },
+    selectedText: {
+      color: '#666',
+      fontSize: 16,
+    },
+    modalContainer: {
+      flex: 1,
+      paddingTop: 50,
+      paddingHorizontal: 20,
+    },
+    modalTitle: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+      color: '#333',
+      textAlign: 'center',
+    },
+    modalOption: {
+      paddingVertical: 15,
+      borderBottomWidth: 1,
+      borderBottomColor: '#ddd',
+    },
+    modalOptionText: {
+      fontSize: 18,
+      color: '#333',
+    },
+    selectedOption: {
+      backgroundColor: '#E6F2FF',
+    },
+    selectedOptionText: {
+      color: '#00F',
+    },
+    checkmarkIcon: {
+      position: 'absolute',
+      right: 10,
+    },
+    modalCloseButton: {
+      marginTop: 20,
+      alignSelf: 'center',
+      backgroundColor: '#FF0000',
+      borderRadius: 25,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+    },
+    modalCloseButtonText: {
+      fontSize: 18,
+      color: '#FFF',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  });
+  
+  
+  
 
 export default SettingComponent;
